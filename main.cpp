f6e96ef1457bc008334dca25807dcdd690c801e8
xwtr <42028912+xwtr@users.noreply.github.com>
2020-03-07 20:37:31 +0330

    Add files via upload
    :)

---
+++ main.cpp
@@ -0,0 +1,130 @@
+//#include <iostream>
+////TODO::difine:)
+//#define ___ std::cout<<
+//#define __ =
+//#define _ <<
+////using namespace std;
+//int main() {
+///*    int a=123,e, *b,c,*d,f,**g,h,t,i,ar[a];char v,arr[a];
+//    char v='s';
+//    char g='g';
+//
+//    cout<<int('gk');
+//    return 0;*/
+//int a=123,b=0x4564,c,d,f,g,h,*t,k,m,n,o,r=a&b/*,*u,*t,**y*/;
+//for(int i=0;i<20;++i){if(r){t=&r;___ char((b%93)+33);
+//        d=a*b;f=b&d;c=d|a;c<<f;g=c^d;h=r+g;r*=r+d;goto l;
+//___ char((d%99)+33);I:k=~(h|f)*'@';___ char((d%99)+33);___ char(((a+r)%93)+33);goto l1;___ char((b%93)+33);}if(r==2){    ___ char((o%99)+33);
+//        ___ char((r%93)+33);
+//    l:o=g<<2;m=o&k;r=m<<2; /*goto L*/goto I1;}
+//else{I1:h=2*0x5675;___ char((o%99)+33);
+//        n=0xABCDF;___ char(((a+r)%93)+33);___ char((n%93)+33);    ___ char(((r+b)%93)+33);
+//
+//        ;*t=n&h;goto I;___ char((m%99)+33);l1:g=~(~c^f);}}
+////
+////    cout<< d ;
+//
+//
+//
+//___ char((b%93)+33);
+//___ char(((c*0x1024)%99)+27);    ___ char((r%93)+33);
+//
+//    ___ char((f%93)+33);
+//    ___ char((g%93)+33);
+//    ___ char((h%99)+33);
+//    ___ char((m%99)+33);
+//    ___ char((n%93)+33);
+//    ___ char((r%93)+33);
+//    ___ char((o%99)+33);
+//    ___ char((*t%99)+33);
+//
+//
+//}
+//
+
+/*
+
+#include <iostream>
+#define ___ std::cout<<
+#define __  __
+#define _ <<
+
+int main[){int a __ 123,b=0x4564,c,d,f,g,h,k,m,n,o,r=a&b*/
+/*,*u,*t,**y*//*
+;
+    for_aint i=0;i<10;++i){if(r){d=a*b;___ char((b%93)+33);f=b&d;c=d|a;
+            ___ char(((c*0x1024)%99)+27);c<<f*i;g=c^d;h=r+g;___ char(((r+c)%93)+33);
+            r*=r+d;goto l;I:k=~(h|f)*'@';goto l1;___ char((m%93)+33);}if(r==2)
+        {l:o=g<<2*i;m=o&k*i;r=m<<2;goto I1;}else{I1:h=2*0x5675<<i;n=0xABCDF>>i;
+            ___ char(((a+i)%93)+33);goto I;___ char((m%93)+33);l1:g=~(~c^f)*i;}
+        ___ char((b%93)+33)_ char(((c*0x1024)%93)+27)_ char((f%93)+33);
+        ___ char((h%93)+33)_ char((n%93)+33);___ char((r%93)+33);
+        ___ char((o%93)+33);}___ d;}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream>/************InTheNameOfGod*****************************/
+#define ___ std::cout<</*******************Asoon Tarin code momkene...*****/
+#define __ =///Hame ino hal mikonan...ino velesh kon akhar kar bia soraghesh
+#define _ <<</**************ma faghat vase Nahar va paziraei omadim...*****/
+#define O goto////////////////////////Makhsosan Abmive---Albate age bedan...
+#define double char////////////////boro code haye dige ro beshkan...////////
+#define _a (/////in akhare kar hal mishe---/////////////////////////////////
+#define _a_ )///////movafagh bashid...//////////////////////////////////////
+#define __a_ }//////////////////////////////////////////////////////////////
+
+int fun (int a ){int b __ 0x44,c,d,f,g,h,y,w ,r __ a-b;long int n,k,m,o  ;
+for _a int i __ y __ n __ k   __ m __ o=0;i<2;++i _a_ {if _a       r) {d __
+    a+b*r;___ double _a  _a _a b+d _a_  % 93)+33);f __ b-d;c __ d/a;/* ___ double
+ * _a/*a*//*(  _a c*0x1024 _a_ %99)+27 _a_ */;c =f*i%a;g __ c*   d;h __ r+ g;
+    ___ double     _a  _a  _a r+c)%93)+33);w __ 0;r*=r+d;O l;      ___ d; ___ f;
+    I:k __ _a -1* _a b+f) +n-0xA _a_  *'@';O l1;___     double _a  _a m%93 _a_ +
+                                                                           33 _a_ ;___ c ;}if(r==2){l:o __ g+=2*      i;m __ k*i;r       __ m %2;O I1;}
+else{I1:h __ 2*0x25+i  ;n __ 0xABD+i;O I;___ double     ( _a  _a c* 0x1024)%
+    93)+27); ___ double _a _a f%93)+33)  ;l1:g __         -1* _a -1*c*f _a_ *i;}
+/*___ double _a  _a*//** h%93 _a_ +33 _a_  ;___ double _a  _a r%93)+33);*/};
+___ double _a  _a  _a a+r+b+k  _a_ %0x5D _a_ +33 _a_;return 0;}/*h=*t+a/b*///
+
+int main(){
+    for (int i = 0; i < 999; ++i) {
+        fun(i);
+        std::cout<<'\n';
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
