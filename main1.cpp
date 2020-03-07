5a1f771d40f86787e7f60b731db0c0116a2c48c0
xwtr <42028912+xwtr@users.noreply.github.com>
2020-03-07 20:39:05 +0330

    Add files via upload

---
+++ main1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>/************InTheNameOfGod*****************************/
+#define ___ std::cout<</*******************Asoon Tarin code momkene...*****/
+#define __ =///Hame ino hal mikonan...ino velesh kon akhar kar bia soraghesh
+#define _ <</**************ma faghat vase Nahar va paziraei omadim...******/
+#define O goto////////////////////////Makhsosan Abmive---Albate age bedan...
+#define double char////////////////boro code haye dige ro beshkan...////////
+#define _a (/////in akhare kar hal mishe---/////////////////////////////////
+#define _a_ )///////movafagh bashid...//////////////////////////////////////
+int main(){int a __ 123,b __ 0x44,c,d,f,g,h,y,w ,r __ a&b;long int n,k,m,o;
+for _a int i __ y __ n __ k __ g __ h  __ m __ o=0;i<3;++i) {if _a r) {d __
+a+b*r;___ double _a  _a b% 93)+33);f __ b&d;c __ d|a; /*   ___ double _a/**/
+/*(  _a c*0x1024 _a_ %99)+27 _a_ */;c _ f*i;g __ c^d;h __ r+g;
+___ double     _a  _a  _a r+c)%93)+33);w __ 0;r*=r+d;O l;___ d; ___ f;
+I:k __ _a ~ _a b&f) +n-0xA _a_  *'@';O l1;___ double _a  _a m%93 _a_ +
+33 _a_ ;___ c ;}if(r==2){l:o __ g>>2*i;m __ o&k*i;r __ m _ 2;O I1;}
+else{I1:h __ 2*0x25<<i  ;n __ 0xABD>>i;;O I;___ double( _a  _a c* 0x1024)%
+93)+27); ___ double _a _a f%93)+33);l1:g __              ~ _a ~c^f _a_ *i;} ;
+/*___ double _a  _a*//** h%93 _a_ +33 _a_   ;___ double _a  _a r%93)+33);*/};
+___ double _a  _a  _a a+r+b+k  _a_ %93 _a_ +33 _a_;}/*h=*t+a/b*//*:)//****/
+////SALAVAT...:)/////////////////////////////////////////////////////////////
+
+
